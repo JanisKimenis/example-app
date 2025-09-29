ad62efb74f895c9c5b746f089846ef24c55d0f5d
you <you@gmail.com>
2025-09-29 10:50:41 +0300

    second

---
+++ resources/views/submit.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $_POST["name"]; ?>
+    <?php echo $_POST["surname"]; ?>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/form', function () {
+    return view('form');
+});
+
+Route::post('/submit', function (Request $request) {
+    // Get the form data
+    $name = $request->input('name');
+    $surname = $request->input('surname');
+
+    // Process the data (store, validate, etc.)
+
+    return (response()->json([
+        'name' => $name,
+        'surname' => $surname
+         
+    ]));
+    
 });
