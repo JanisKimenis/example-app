0df648eec1071a8f44b9c0c8f91efb45b3fbd44a
you <you@gmail.com>
2025-09-30 09:36:47 +0300

    /posts izvada manu vārdu

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/posts', 
+    [PostController::class, 'index']
+);
